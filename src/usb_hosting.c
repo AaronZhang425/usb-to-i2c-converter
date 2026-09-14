acdd8fc3f0e4621393f559d1051744b9f90d9691
Aaron Zhang <aaronbxzhang@gmail.com>
2026-09-13 20:02:45 -0400

    Finish remove_device_descriptor_cb

--- src/usb_hosting.c
+++ src/usb_hosting.c
@@ -86,6 +86,13 @@ void remove_device_descriptor_cb(tuh_xfer_t *xfer) {
 
     }
 
+    struct event event = {
+        .event_data_size = sizeof(tuh_xfer_t),
+        .event_data = xfer,
+        .event_type = USB_RELEASE
+    };
+
+    queue_add_by_event(event_queue, event);
 
 }
 
